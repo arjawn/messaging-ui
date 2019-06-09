f18d102c9a46f6fd907928e6166304d7e9c27097
Arjun Mahadevan <mahadevanarjun21@gmail.com>
2019-06-09 14:31:14 -0700

    added message bubble

--- components/Status.js
+++ components/Status.js
@@ -19,16 +19,34 @@ export default class Status extends React.Component {
 
         const backgroundColor = isConnected ? 'white' : 'red';
 
-        // eslint-disable-next-line no-console
-        console.log('here');
+        const statusBar = (
+            <StatusBar
+                backgroundColor={backgroundColor}
+                barStyle={isConnected ? 'dark-content' : 'light-content'}
+                animated={false}
+            />
+        );
+
+        const messageContainer = (
+            <View style={styles.messageContainer} pointerEvents={'none'}>
+                {statusBar}
+                {!isConnected && (
+                    <View style={styles.bubble}>
+                        <Text style={styles.text}>No network connection</Text>
+                    </View>
+                )}
+            </View>
+        );
 
         if (Platform.OS === 'ios') {
-            // eslint-disable-next-line no-console
-            console.log('here');
-            return <View style={[styles.status, { backgroundColor }]} />;
+            return (
+                <View style={[styles.status, { backgroundColor }]}>
+                    {messageContainer}
+                </View>
+            );
         }
 
-        return null; // Temporary
+        return messageContainer;
     }
 }
 
@@ -38,5 +56,23 @@ const styles = StyleSheet.create({
     status: {
         zIndex: 1, // indicates this View should be drawn on top of other content
         height: statusHeight
+    },
+    messageContainer: {
+        zIndex: 1,
+        position: 'absolute',
+        top: statusHeight + 20,
+        right: 0,
+        left: 0,
+        height: 80,
+        alignItems: 'center'
+    },
+    bubble: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+        backgroundColor: 'red'
+    },
+    text: {
+        color: 'white'
     }
 });
